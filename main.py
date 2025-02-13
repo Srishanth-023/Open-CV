3691e9c87746370ec5cdb78d21c0f64d980401bb
SRISHANTH P M <srishanth232007@gmail.com>
2025-02-13 10:54:18 +0530

    A

--- main.py
+++ main.py
@@ -85,5 +85,8 @@ import os
 # COLOR SPACES
 
 img = cv2.imread(os.path.join(',', 'Images', 'rabbit.jpg'))
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+cv2.imshow('RABBIT_RGB', img_rgb)
 cv2.imshow('RABBIT', img)
 cv2.waitKey(0)
